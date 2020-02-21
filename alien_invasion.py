7ce990133af0c769da8e8646cffa1401c4a225cf
AIB <madhmadmad@hotmail.com>
2020-02-22 00:35:25 +0300

    added bullets and firing but got error Bullet has no attribute drawbullet

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,10 @@
 import pygame
+from pygame.sprite import Group
 
 from Game.settings import Settings
 from Game.ship import Ship
 import Game.game_functions as gf
+
 
 def run_game():
     # Initialize game and create a screen object.
@@ -15,12 +17,16 @@ def run_game():
     # make a ship
     ship = Ship(ai_settings, screen)
 
+    # make a group to store te bullets in
+    bullets = Group()
+
     # start main loop
     while True:
         # keyboard and mouse events
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
