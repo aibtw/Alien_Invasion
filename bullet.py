e5caeefb86bb3f8a8507011d0a49a25e0e67499b
AIB <madhmadmad@hotmail.com>
2020-02-22 00:45:06 +0300

    fixed the error in previous version, it was indent in file bullet for the last two methods

--- bullet.py
+++ bullet.py
@@ -20,13 +20,13 @@ class Bullet(Sprite):
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
 
-        def update(self):
-            """Move the bullet pu the screen"""
-            # update the decimal position of the bullet
-            self.y -= self.speed_factor
-            # update the rect position
-            self.rect.y = self.y
+    def update(self):
+        """Move the bullet pu the screen"""
+        # update the decimal position of the bullet
+        self.y -= self.speed_factor
+        # update the rect position
+        self.rect.y = self.y
 
-        def drawbullet(self):
-            """Draw the bullet to the screen"""
-            pygame.draw.rect(self.screen, self.color, self.rect)
+    def drawbullet(self):
+        """"Draw the bullet to the screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
